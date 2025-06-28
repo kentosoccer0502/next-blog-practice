725053f4bdc11901a809adceab57db5c729860b3
kentosoccer0502 <kentosoccer0502@gmail.com>
2025-06-28 16:36:01 +0900

    add: Dropdownでログアウト機能

---
+++ src/components/layouts/Setting.tsx
@@ -0,0 +1,34 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { signOut } from "@/auth"
+import { Button } from "@/components/ui/button"
+import { Session } from "next-auth"
+
+export default function Setting({session}: {session: Session}) {
+    const handleLogout = async () => {
+        'use server'
+        await signOut()
+    }
+  return (
+    <DropdownMenu>
+    <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="font-medium cursor-pointer">
+            {session.user?.name}
+        </Button>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent align="end" className="w-48">
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={handleLogout} className="cursor-pointer">
+            ログアウト
+        </DropdownMenuItem>
+    </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
