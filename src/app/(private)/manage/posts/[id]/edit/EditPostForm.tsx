2455c4c0569a2d5a66c2113fdb09ae737d642d51
kentosoccer0502 <kentosoccer0502@gmail.com>
2025-06-30 00:05:03 +0900

    add: 画像のプレビュー機能、表示非表示ラジオボタン

--- src/app/(private)/manage/posts/[id]/edit/EditPostForm.tsx
+++ src/app/(private)/manage/posts/[id]/edit/EditPostForm.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useState, useActionState } from "react"
+import { useState, useActionState, useEffect } from "react"
 import ReactMarkdown from "react-markdown"
 import remarkGfm from "remark-gfm"
 import rehypeHighlight from "rehype-highlight"
@@ -9,12 +9,26 @@ import { Label } from '@/components/ui/label'
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import { createPost } from "@/lib/actions/createPost"
+import Image from "next/image"
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
 
+type EditPostFormProps = {
+    post: {
+        id: string;
+        title: string;
+        content: string;
+        topImage?: string | null;
+        published: boolean
+    }
+}
 
-export default function CreatePage() {
-    const [content, setContent] = useState('')
+export default function EditPostForm({post} : EditPostFormProps) {
+    const [content, setContent] = useState(post.content)
     const [contentLength, setContentLength] = useState(0)
     const [preview, setPreview] = useState(false)
+    const [title, setTitle] = useState(post.title)
+    const [published, setPublished] = useState(post.published)
+    const [imagePreview, setImagePreview] = useState(post.topImage)
 
     const [state, formAction] = useActionState(createPost, {
             success: false, errors: {}
@@ -26,13 +40,31 @@ export default function CreatePage() {
         setContentLength(value.length)
     }
 
+    const handleImageChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files?.[0]
+        if (file) {
+            const previewUrl = URL.createObjectURL(file)
+            setImagePreview(previewUrl)
+        }
+    }
+
+    useEffect(() => {
+        return () => {
+            if(imagePreview && imagePreview !== post.topImage) {
+                URL.revokeObjectURL(imagePreview)
+            }
+        }
+    }, [imagePreview, post.topImage])
+
   return (
     <div className="container ms-auto mt-10">
       <h1 className="text-2xl font-bold mb-4">新規記事投稿(Markdown対応)</h1>
       <form action={formAction} className="space-y-4">
         <div>
             <Label htmlFor="title">タイトル</Label>
-            <Input type="text" id="title" name="title" placeholder="タイトルを入力してください" />
+            <Input 
+                type="text" id="title" name="title" placeholder="タイトルを入力してください"
+                value={title} onChange={(e) => setTitle(e.target.value)} />
             {state.errors.title && (
                         <p className='text-red-500 text-sm mt-1'>{state.errors.title.join(',')}</p>
                     )}
@@ -44,7 +76,21 @@ export default function CreatePage() {
                 id="topImage"
                 accept="image/*"
                 name="topImage"
+                onChange={handleImageChange}
             />
+            {imagePreview && (
+                <div className="mt-2">
+                    <Image
+                        src={imagePreview}
+                        alt ={post.title}
+                        width={0}
+                        height={0}
+                        sizes="200px"
+                        className="w-[200px]"
+                        priority 
+                        />
+                </div>
+            )}
             {state.errors.topImage && (
                         <p className='text-red-500 text-sm mt-1'>{state.errors.topImage.join(',')}</p>
                     )}
@@ -77,7 +123,19 @@ export default function CreatePage() {
                 >{content}</ReactMarkdown>
             </div>
         )}
-        <Button type="submit" className="bg-blue-500 text-white px-4 py-2 rouded">投稿する</Button>
+        <RadioGroup value={published.toString()} name="published" onValueChange={(value) => setPublished(value === 'true')}>
+        <div className="flex items-center space-x-2">
+            <RadioGroupItem value="true" id="published-one" />
+            <Label htmlFor="published-one">表示</Label>
+        </div>
+        <div className="flex items-center space-x-2">
+            <RadioGroupItem value="false" id="published-two" />
+            <Label htmlFor="published-two">非表示</Label>
+        </div>
+        </RadioGroup>
+        <Button type="submit" className="bg-blue-500 text-white px-4 py-2 rouded">更新する</Button>
+        <input type="hidden" name="postId" value={post.id} /> // 記事ID
+        <input type="hidden" name="oldImageUrl" value={post.topImage || ''} />
       </form>
     </div>
   )
