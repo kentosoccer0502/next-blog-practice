fcbc38f9bceb167cc0c76a5fc6c9cf5faf8571ce
kentosoccer0502 <kentosoccer0502@gmail.com>
2025-06-29 11:02:49 +0900

    add: sessionにuser.id追加処理、dashboard一覧

--- src/app/(private)/dashboard/page.tsx
+++ src/app/(private)/dashboard/page.tsx
@@ -1,9 +1,12 @@
 import { getOwnPosts } from "@/lib/ownPost"
 import { auth } from "@/auth"
+import PostDropdownMenu from "@/components/post/PostDropdownMenu"
+import { Button } from '@/components/ui/button'
 
 export default async function DashboardPage() {
   const session = await auth()
   const userId = session?.user?.id
+  console.log(session)
 
   if(!session?.user?.email || !userId) {
     throw new Error('不正なリクエストです')
@@ -11,8 +14,37 @@ export default async function DashboardPage() {
   const posts = await getOwnPosts(userId)
 
   return (
-    <div>
-      dashboard
+    <div className="p-4">
+      <div className="flex justify-between">
+        <h1 className="text-2xl font-bold-m4 mb-4">記事一覧</h1>
+        <Button>新規記事作成</Button>
+      </div>
+      <table className="table-auto w-full border-collapse border">
+        <thead>
+          <tr className="bg-gray-100">
+            <th className="border p-2 text-center">タイトル</th>
+            <th className="border p-2 text-center">表示 / 非表示</th>
+            <th className="border p-2 text-center">更新日時</th>
+            <th className="border p-2 text-center">操作</th>
+          </tr>
+        </thead>
+        <tbody>
+          { posts.map((post) => (
+            <tr key={post.id}>
+              <td className="border p-2">{post.title}</td>
+              <td className="border p-2 text-center">
+                {post.published ? "表示" : "非表示"}
+              </td>
+              <td className="border p-2 text-center">
+                {new Date(post.updatedAt).toLocaleDateString()}
+              </td>
+              <td className="border p-2 text-center">
+                <PostDropdownMenu postId={post.id} />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   )
 }
